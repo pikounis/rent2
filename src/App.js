38fde2727bd200a45eb3c01db1849af2a847c5e3
Anastasios Pikounis <anastasiospikounis@gmail.com>
2023-12-02 01:00:59 +0000

    changed it so the second input has the value of the first input after the math operation and vice versa so the result text is not needed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React, { useState, useMemo } from 'react';
-// import logo from './logo.svg';
+import React, { useState} from 'react';
 import logo2 from './home.png';
 import './App.css';
 
 function App() {
-  const [weekly, setWeekly] = useState();
-  const [monthly, setMonthly] = useState();
-
-  const calculateWeeklyToMonthly = useMemo(() => {
-    return ((weekly * 52) / 12);
-  }, [weekly]);
-
-  const calculateMonthlyToWeekly = useMemo(() => {
-    return ((monthly * 12) / 52);
-  }, [monthly]);
+  const [weekly, setWeekly] = useState('');
+  const [monthly, setMonthly] = useState('');
 
   const handleWeeklyChange = (event) => {
-    setWeekly(event.target.value);
+    const weeklyValue = event.target.value;
+    setWeekly(weeklyValue);
+    if (weeklyValue) {
+      setMonthly(((weeklyValue * 52) / 12).toFixed());
+    } else {
+      setMonthly('');
+    }
   };
 
   const handleMonthlyChange = (event) => {
-    setMonthly(event.target.value);
+    const monthlyValue = event.target.value;
+    setMonthly(monthlyValue);
+    if (monthlyValue) {
+      setWeekly(((monthlyValue * 12) / 52).toFixed());
+    } else {
+      setWeekly('');
+    }
   };
 
   return (
@@ -28,25 +31,21 @@ function App() {
         <header className="App-header">
           <div>
             <img src={logo2} className="App-logo" alt="logo" />
-            <h3>Weekly to Monthly</h3>
-            {/*{weekly && <h1>£ {calculateWeeklyToMonthly.toFixed()}</h1>}*/}
-            {weekly && <h1> £{weekly} per week = £{calculateWeeklyToMonthly.toFixed()} per month</h1>}
+            <h3>Weekly</h3>
             <input
                 value={weekly}
                 onChange={handleWeeklyChange}
-                type="number"
+                type="tel"
                 pattern="\d*"
             />
           </div>
 
           <div className="div-space">
-            <h3>Monthly to Weekly</h3>
-            {/*{monthly && <h1> £ {calculateMonthlyToWeekly.toFixed()}</h1>}*/}
-            {monthly && <h1> £{monthly} per month = £{calculateMonthlyToWeekly.toFixed()} per week</h1>}
+            <h3>Monthly</h3>
             <input
                 value={monthly}
                 onChange={handleMonthlyChange}
-                type="number"
+                type="tel"
                 pattern="\d*"
             />
           </div>
